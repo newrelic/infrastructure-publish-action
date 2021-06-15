cb126ae05689d4a663bf2bef6f4b166c40c49828
Grigorii Merkushev <brushknight@gmail.com>
2021-06-15 09:16:48 +0200

    fix: split access point and mirror url (#104)

--- publisher/publisher_test.go
+++ publisher/publisher_test.go
@@ -447,21 +447,23 @@ func Test_generateAptSrcRepoUrl(t *testing.T) {
 
 func Test_parseAccessPointHost(t *testing.T) {
 	tests := []struct {
-		name            string
-		accessPointHost string
-		expectedUrl     string
+		name              string
+		accessPointHost   string
+		expectedUrl       string
+		expectedMirrorUrl string
 	}{
-		{"empty value fallback to prod", "", accessPointProduction},
-		{"production placeholder", "production", accessPointProduction},
-		{"staging placeholder", "staging", accessPointStaging},
-		{"testing placeholder", "testing", accessPointTesting},
-		{"fixed url", "https://www.some-bucket-url.com", "https://www.some-bucket-url.com"},
+		{"empty value fallback to prod", "", accessPointProduction, mirrorProduction},
+		{"production placeholder", "production", accessPointProduction, mirrorProduction},
+		{"staging placeholder", "staging", accessPointStaging, accessPointStaging},
+		{"testing placeholder", "testing", accessPointTesting, accessPointTesting},
+		{"fixed url", "https://www.some-bucket-url.com", "https://www.some-bucket-url.com", "https://www.some-bucket-url.com"},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			srcRepo := parseAccessPointHost(tt.accessPointHost)
+			srcRepo, mirrorHost := parseAccessPointHost(tt.accessPointHost)
 			assert.Equal(t, tt.expectedUrl, srcRepo)
+			assert.Equal(t, tt.expectedMirrorUrl, mirrorHost)
 		})
 	}
 }
