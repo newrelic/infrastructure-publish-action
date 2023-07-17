5644e5fb4d7719297da3081a7c639088fc02c1ec
Paolo Gallina <paologallina1992@gmail.com>
2023-07-17 15:32:30 +0200

    feat(hack): remove unused software to free up space

--- action-run.sh
+++ action-run.sh
@@ -14,6 +14,12 @@ if [ "${CI}" = "true" ]; then
   set -e
 fi
 
+## Freeing space since 14GB are not enough anymore
+df -h
+echo "Deleting android, dotnet, haskell, CodeQL, Python, swift to free up space"
+sudo rm -rf /usr/local/lib/android /usr/share/dotnet /usr/local/.ghcup /opt/hostedtoolcache/CodeQL /opt/hostedtoolcache/Python /usr/share/swift
+df -h
+
 # run docker container to perform all actions inside.
 # $( pwd ) is mounted on /srv to enable grabbing packages
 # from the host machine instead of downloading them from GH,
@@ -53,3 +59,7 @@ docker run --platform linux/amd64 --rm \
         -e APT_SKIP_MIRROR \
         newrelic/infrastructure-publish-action \
         "$@"
+
+# Verifying how much space is still available
+echo "After running the upload command this is the current status"
+df -h
